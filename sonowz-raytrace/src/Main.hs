bfca8071561b92aa48257cd819287717cf43ba53
dnsdhrj <dnsdhrj123@gmail.com>
2019-10-10 13:50:48 +0000

    Add command line options

--- sonowz-raytrace/src/Main.hs
+++ sonowz-raytrace/src/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
 import           Relude
-import           System.IO
 import           Control.Applicative
 import           Control.Concurrent
 import           Network.Wai.Handler.Warp
+import           Options.Applicative
 import           Servant
+import qualified System.IO as IO
 
 import qualified Sonowz.Raytrace.Types.API     as API
 import qualified Sonowz.Raytrace.Daemon as RTDaemon
@@ -23,13 +24,22 @@ app wsdata = serve api (server wsdata)
 
 -- TODO: refactor using https://docs.servant.dev/en/stable/cookbook/using-custom-monad/UsingCustomMonad.html
 
+portP :: Parser Port
+portP = argument (auto >>= checkPort) (metavar "PORT" <> value 80) where
+  checkPort port = if 0 < port && port < 90000 then return port else empty
+
+opts :: ParserInfo Port
+opts = info (helper <*> portP) ( fullDesc <> progDesc "Raytrace backend server")
+
 -- 'Raytrace.Websocket.WSData' is mutable IPC object. (between Daemon and Server)
 -- 'Raytrace.Daemon' receives input, runs raytrace assignment program, and send image file ID.
 main :: IO ()
 main = do
-  hSetBuffering stdout LineBuffering -- For debugging
-  hSetBuffering stderr LineBuffering
+  IO.hSetBuffering stdout IO.LineBuffering -- For debugging
+  IO.hSetBuffering stderr IO.LineBuffering
 
   wsdata <- RTWebsocket.initWSData
   _ <- forkFinally (RTDaemon.main wsdata) RTDaemon.onQuit
-  run 8080 (app wsdata)
+
+  port <- execParser opts
+  run port (app wsdata)
