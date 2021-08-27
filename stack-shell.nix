d07510852b4289be7d558e530a80f07920cb0e5d
sonowz <dnsdhrj123@gmail.com>
2021-08-27 21:34:08 +0900

    Clean nix files

--- stack-shell.nix
+++ stack-shell.nix
@@ -1,14 +1,10 @@
 { ghc }:
 with import <nixpkgs> {};
 
-# Nix build environment is included for making images
 haskell.lib.buildStackProject {
   inherit ghc;
   name = "sonowz-backend";
-  # TODO: do not hard-code NIX_PATH
-  NIX_PATH = "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos";
   buildInputs = [
-    nix
     postgresql
     zlib
   ];
