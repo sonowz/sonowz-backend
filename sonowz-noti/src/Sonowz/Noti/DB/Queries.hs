4a27ab20a0fe485cd2aaf8366719136428e84aaa
sonowz <dnsdhrj123@gmail.com>
2022-01-11 20:46:38 +0900

    sonowz-noti DB queries

---
+++ sonowz-noti/src/Sonowz/Noti/DB/Queries.hs
@@ -0,0 +1,74 @@
+module Sonowz.Noti.DB.Queries
+  ( insertNotification
+  , selectOneNotification
+  , deleteNotificationByUid
+  ) where
+
+import Database.PostgreSQL.Simple (Connection)
+import Database.PostgreSQL.Simple.Transaction (withTransaction)
+import Opaleye
+
+import Sonowz.Noti.DB.Types
+import Sonowz.Noti.Imports
+import Sonowz.Noti.Notification.Types (Notification(..))
+
+-- Table declarations --
+
+notificationTable :: NotificationTable
+notificationTable = table "notification" (pNotification notificationFields)
+
+notificationFields = Notification'
+  { uid         = tableField "uid"
+  , _type       = tableField "type"
+  , title       = tableField "title"
+  , body        = tableField "body"
+  , createdTime = tableField "created_time"
+  }
+
+-- Public Interfaces --
+
+insertNotification :: HasCallStack => Connection -> Notification -> IO Bool
+insertNotification conn noti = withTransaction conn $ checkSuccess <$> insertResult where
+  insertResult = runInsert_ conn (qInsertNotification notificationTable noti)
+  checkSuccess 1 = True
+  checkSuccess _ = False
+
+selectOneNotification :: HasCallStack => Connection -> IO (Maybe (Uid, Notification))
+selectOneNotification conn = withTransaction conn $ listToMaybe <$> (construct <<$>> selectResult) where
+  selectResult = runSelect conn (qSelectMinNotification notificationTable)
+  construct :: NotificationHask -> (Uid, Notification)
+  construct Notification' {..} = (uid, Notification _type title body)
+
+deleteNotificationByUid :: HasCallStack => Connection -> Uid -> IO Bool
+deleteNotificationByUid conn uid = withTransaction conn $ checkSuccess <$> deleteResult where
+  deleteResult = runDelete_ conn (qDeleteNotificationByUid notificationTable uid)
+  checkSuccess 1 = True
+  checkSuccess _ = False
+
+-- Private Functions --
+
+notiToWriteField :: Notification -> NotificationFieldW
+notiToWriteField Notification {..} = Notification'
+  { uid         = Nothing
+  , _type       = toFields notificationType
+  , title       = toFields notificationTitle
+  , body        = toFields notificationBody
+  , createdTime = Nothing
+  }
+
+-- Queries --
+
+qInsertNotification :: NotificationTable -> Notification -> Insert Int64
+qInsertNotification table noti = Insert
+  { iTable      = table
+  , iRows       = [notiToWriteField noti]
+  , iReturning  = rCount
+  , iOnConflict = Just DoNothing
+  }
+
+qSelectMinNotification :: NotificationTable -> Select NotificationFieldR
+qSelectMinNotification table = limit 1 $ orderBy (asc uid) $ selectTable table
+
+qDeleteNotificationByUid :: NotificationTable -> Uid -> Delete Int64
+qDeleteNotificationByUid table targetUid =
+  Delete { dTable = table, dWhere = \row -> uid row .== toFields targetUid, dReturning = rCount }
---
+++ sonowz-noti/src/Sonowz/Noti/DB/Types.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Sonowz.Noti.DB.Types where
+
+import Data.Profunctor.Product.Default (Default(..))
+import Data.Profunctor.Product.TH (makeAdaptorAndInstance)
+import Data.Time (UTCTime)
+import qualified Database.PostgreSQL.Simple.FromField as FF
+import Opaleye
+import Sonowz.Core.DB.Utils
+import Sonowz.Noti.Imports
+import Sonowz.Noti.Notification.Types (NotificationBody, NotificationType)
+
+newtype Uid = Uid Int
+  deriving (Eq, Show, Read)
+
+data Notification' c1 c2 c3 c4 c5 = Notification'
+  { uid         :: c1
+  , _type       :: c2
+  , title       :: c3
+  , body        :: c4
+  , createdTime :: c5
+  }
+
+type NotificationHask = Notification' Uid NotificationType Text NotificationBody UTCTime
+type NotificationFieldW
+  = Notification' -- Write fields
+      (Maybe (Field SqlInt4))
+      (Field SqlText)
+      (Field SqlText)
+      (Field SqlText)
+      (Maybe (Field SqlTimestamptz))
+type NotificationFieldR
+  = Notification' -- Read fields
+      (Field SqlInt4)
+      (Field SqlText)
+      (Field SqlText)
+      (Field SqlText)
+      (Field SqlTimestamptz)
+type NotificationTable = Table NotificationFieldW NotificationFieldR
+
+-- Opaleye-related stuffs --
+$(makeAdaptorAndInstance "pNotification" ''Notification')
+deriving via Int instance QueryRunnerColumnDefault SqlInt4 Uid
+instance QueryRunnerColumnDefault SqlText NotificationType where
+  defaultFromField = fieldQueryRunnerColumn
+instance QueryRunnerColumnDefault SqlText NotificationBody where
+  defaultFromField = fieldQueryRunnerColumn
+instance Default Constant Uid (Column SqlInt4) where
+  def = coerce (def :: Constant Int (Column SqlInt4))
+instance Default Constant NotificationType (Column SqlText) where
+  def = Constant (sqlStrictText . show)
+instance Default Constant NotificationBody (Column SqlText) where
+  def = Constant (sqlStrictText . show)
+instance FF.FromField NotificationType where
+  fromField = fromFieldSimple (readEither . decodeUtf8)
+instance FF.FromField NotificationBody where
+  fromField = fromFieldSimple (readEither . decodeUtf8)
