f7592660928eafa15fc6e89ab4e7063ec8cda374
sonowz <dnsdhrj123@gmail.com>
2022-03-09 13:06:31 +0900

    Introduce flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Sonowz Backend";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        version = "1.0.0";
+        name = "sonowz-backend";
+
+        pkgs = nixpkgs.legacyPackages.${system};
+        requirements = pkgs.callPackage ./requirements.nix {};
+
+        # TODO: remove hardcoded path
+        binPath = /home/sonowz/sonowz-backend/bin;
+        
+        # Build command : nix build --impure
+        app = pkgs.stdenv.mkDerivation {
+          inherit name;
+          inherit version;
+          src = binPath;
+          nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+          propagatedBuildInputs = requirements;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r . $out/bin
+          '';
+        };
+      in
+      {
+        inherit app;
+        defaultPackage = app;
+
+        devShell = pkgs.mkShell {
+          # buildInputs = requirements;
+          LD_LIBRARY_PATH = "${pkgs.glibc}/lib/";
+        };
+      }
+    );
+}
---
+++ requirements.nix
@@ -0,0 +1,2 @@
+{ gmp, glibc, libffi, libpqxx, ncurses, postgresql, zlib }:
+[ gmp  glibc  libffi  libpqxx  ncurses  postgresql  zlib ]
