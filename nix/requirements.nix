bf4ed321fd462e4e64810c341c187cbdbaeabb97
sonowz <dnsdhrj123@gmail.com>
2025-05-11 19:58:30 +0900

    Fix runtime dependency

--- nix/requirements.nix
+++ nix/requirements.nix
@@ -1,2 +1,2 @@
-{ flac, icu, libffi, libpqxx, ncurses, postgresql, taglib, zlib }:
-[ flac  icu  libffi  libpqxx  ncurses  postgresql  taglib  zlib ]
+{ elfutils, flac, gmp, icu, libffi, libpqxx, ncurses, postgresql, taglib, zlib }:
+[ elfutils  flac  gmp  icu  libffi  libpqxx  ncurses  postgresql  taglib  zlib ]
