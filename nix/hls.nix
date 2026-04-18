0401e5cd6a478460454e06f4c028525c65823166
sonowz <dnsdhrj123@gmail.com>
2026-04-18 13:07:41 +0900

    Add direnv

--- nix/hls.nix
+++ nix/hls.nix
@@ -1,15 +1,19 @@
 # This is a derivation for building HLS faster
 { lib, haskellPkgs, haskellLib }:
 let
-  fastBuild = drv: drv.overrideScope (hself: hsuper: {
-    mkDerivation = args: hsuper.mkDerivation (args // {
-      doHaddock = false;
-      doCheck = false;
-      doBenchmark = false;
-      doCoverage = false;
-      enableLibraryProfiling = false;
+  fastHaskellPkgs = haskellPkgs.override (old: {
+    overrides = lib.composeExtensions (old.overrides or (_: _: { })) (hself: hsuper: {
+      mkDerivation = args: hsuper.mkDerivation (args // {
+        doHaddock = false;
+        doCheck = false;
+        doBenchmark = false;
+        doCoverage = false;
+        enableLibraryProfiling = false;
+        enableExecutableProfiling = false;
+      });
     });
   });
+
   dynamicBuild = drv: lib.trivial.pipe drv [
     (haskellLib.compose.overrideCabal (old: {
       enableSharedExecutables = true;
@@ -17,8 +21,7 @@ let
     (haskellLib.compose.enableCabalFlag "dynamic")
   ];
 in
-  lib.trivial.pipe haskellPkgs.haskell-language-server [
-    fastBuild
+  lib.trivial.pipe fastHaskellPkgs.haskell-language-server [
     dynamicBuild
   ]
         
